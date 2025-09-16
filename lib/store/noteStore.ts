8db986778489f481954dbfdc09c69a974d5a2748
d-yv <yuri.dubrovin@gmail.com>
2025-09-16 16:44:37 +0300

    fix NoteStore

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import {create} from 'zustand';
 import {persist} from 'zustand/middleware';
-import { CreateNoteParams } from '@/lib/api/api';
+import { CreateNoteParams } from '@/lib/api/clientApi';
 
 type NoteDraftStore = {
     draft: CreateNoteParams;
